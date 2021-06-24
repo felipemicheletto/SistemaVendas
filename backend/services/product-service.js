6deb2549047a3f284fc11188e2ed9c9a9d8fd6d1
reginaldo <reginaldo.sobrinho@codebyte.com.br>
2021-06-24 20:03:08 -0300

    aula 27

--- backend/services/product-service.js
+++ backend/services/product-service.js
@@ -1,3 +1,25 @@
-function getAllProducts() {
-    return 'users'
+function readFile(file, storeLocation) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                localStorage.setItem(storeLocation, rawFile.responseText)                          
+            }
+        }
+    }    
+    rawFile.send(null);
+}
+
+readFile("../../../../../backend/database/produtos.json", 'products')
+
+function getAllProducts() { 
+    return JSON.parse(localStorage.getItem("products"))
+}
+
+function getProductsByName() {
+    return 'products'
 }
--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -4,5 +4,51 @@ $(document).ready(function(){
         localStorage.setItem("userData","")
         window.location.href="../../../index.html"
     })
+    loadProducts();
 });
 
+function loadProducts() {
+    var produtos = getAllProducts()
+    produtos.forEach(element => {
+        var id = element.tableName.replace(" ","")
+        createModal(element.data, id, element.tableName) 
+        createCard(element.tableName, id)
+    });
+}
+
+
+function createCard(title, id) {
+    $(".main").append(
+        `
+        <div class="row">
+            <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+                <span class="card-title">${title}</span>
+                </div>
+                <div class="card-action">
+                <a class="modal-trigger" href="#${id}">Ver Tabela</a>
+                <a href="#">Acessar produto</a>
+                </div>
+            </div>
+            </div>
+        </div>
+        `
+    )
+}
+
+function createModal(data, id, title) {
+    $(".modais").append(
+        `
+        <div id="${id}" class="modal">
+            <div class="modal-content">
+                <h4>${title}</h4>
+                <p>TABELA DE PRODUTOS ${title}</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+            </div>
+        </div> 
+        `
+    )
+}
