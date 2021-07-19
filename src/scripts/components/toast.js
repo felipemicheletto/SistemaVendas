6670ca11f39e1c0324a835736319ad522806f837
cayo261 <85587981+cayo261@users.noreply.github.com>
2021-07-19 16:12:59 -0300

    Toasts e Quebra Linha

--- src/scripts/components/toast.js
+++ src/scripts/components/toast.js
@@ -14,7 +14,19 @@ function openToast(msg, type) {
     }
 
     M.toast({
-        html: msg,
+        html: '<div class="verde">   <i class="material-icons">check</i>  <b>  Success  </b> </div>'  +  msg  + '</div>' ,
+        classes: classe,
+        displayLength: 4000
+    });
+
+    M.toast({
+        html: '<div class="azul">   <i class="material-icons">info</i>  <b>  Info  </b> </div>'  +  msg  + '</div>' ,
+        classes: classe,
+        displayLength: 40000000000000
+    });
+
+    M.toast({
+        html: '<div class="amarelo">   <i class="material-icons">warning</i>  <b>  Warning  </b> </div>'  +  msg  + '</div>' ,
         classes: classe,
         displayLength: 4000
     });
